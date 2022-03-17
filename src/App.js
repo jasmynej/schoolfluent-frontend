e8d7b1d9e8078d4b8fd7041966d83aa9e5a09b80
Jasmyne Jean-Remy <jasmynej14@gmail.com>
2022-03-17 01:02:11 -0400

    css for login and home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from "./sf-logo.png"
+import { useNavigate } from "react-router-dom";
 import './App.css';
 
 function App() {
+  let navigate = useNavigate();
   return (
     <div className="App">
-        <h1>schoolFluent</h1>
-        <img src={logo} />
+        <div className="main-box">
+          <img src={logo} id="logo" />
+          <button onClick={() => {navigate('/login')}} id="login-button">Login</button>
+        </div>
+        
     </div>
   );
 }
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import {authService} from '../services/auth'
 import { useNavigate } from "react-router-dom";
+import "../styles/auth_styles.css"
 function Login(){
     let navigate = useNavigate();
     const [email,setEmail] = useState("")
@@ -35,24 +36,29 @@ function Login(){
         
     }
     return (
-        <div>
-        <h1>login</h1>
-        <form onSubmit={formSubmit}>
-            <div>
-                <input type="radio" value="students" name="student" onChange={typeChange}/>Student
-                <input type="radio" value="teachers-admins" name="teacher-admin" onChange={typeChange}/>Teacher/Admin
-                <input type="radio" value="parents" name="parent" onChange={typeChange}/>Parent
+        <div className='login-container'>
+            <div className='login-box'>
+            <h1>Login</h1>
+            <form onSubmit={formSubmit} className="login-form">
+                <div>
+                    <input type="radio" value="students" name="student" onChange={typeChange}/>Student
+                    <input type="radio" value="teachers-admins" name="teacher-admin" onChange={typeChange}/>Teacher/Admin
+                    <input type="radio" value="parents" name="parent" onChange={typeChange}/>Parent
+                </div>
+                <br/>
+                <label>Email</label>
+                <br/>
+                <input type="text" name="email" onChange={formChange} />
+                <br/>
+                <label>Password</label>
+                <br/>
+                <input type="password" name="password" onChange={formChange}/>
+                <br/>
+                <button type="submit" id="login-button-1">Login</button>
+            </form>
             </div>
-            <br/>
-            <label>email</label>
-            <input type="text" name="email" onChange={formChange} />
-            <br/>
-            <label>password</label>
-            <input type="password" name="password" onChange={formChange}/>
-            <br/>
-            <button type="submit">login</button>
-        </form>
-    </div>
+        </div>
+        
     )
     
 }
